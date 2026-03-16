556149d85fb6bdf6399aaa87fa62e45a989bf046
loannjui <loann.juillerat@heig-vd.ch>
2026-03-16 15:46:59 +0100

    ifuckedup

--- tp1/src/App.vue
+++ tp1/src/App.vue
@@ -1,5 +1,9 @@
 <script setup>
+import { ref } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
+const kelvin = ref(0);
+const celsius = computed(() => kelvin.value - 273.15)
+const fahrenheit = computed(()=> kelvin.value*1.8 -459.67)
 </script>
 
 <template>
